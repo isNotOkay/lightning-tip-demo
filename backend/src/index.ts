a98b9c3fbe5ee3db53939bab9e6d9db15ccc4190
kodenthal <Kai.Odenthal@mikatiming.de>
2019-03-14 11:19:37 +0100

    adjust amount

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,7 +21,7 @@ app.get("/tip", (req, res) => {
     http.post('https://37.221.195.132:8080/v1/invoices', {
         body: {
             memo: `Tip No. ${new Date().getTime()}`,
-            value: 100
+            value: 3500
         },
         headers: {
             'Grpc-Metadata-macaroon': macaroonHexString
